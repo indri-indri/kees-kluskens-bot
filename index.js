b65be6e2a91cdbed3faf812e3f6cf05a7090229f
indri-indri <rutger@codeyellow.nl>
2016-09-29 00:20:15 +0200

    Sort behaviors

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const bot = new TelegramBot(token, {
 	polling: true
 });
 
-require('./behavior/kees.js')(bot);
 require('./behavior/anti-spam.js')(bot);
-require('./behavior/fontys-end.js')(bot);
-require('./behavior/communism.js')(bot);
 require('./behavior/apple.js')(bot);
-require('./behavior/me-irl.js')(bot);
-require('./behavior/sarcasm.js')(bot);
+require('./behavior/communism.js')(bot);
 require('./behavior/doei-he.js')(bot);
+require('./behavior/fontys-end.js')(bot);
 require('./behavior/inb4.js')(bot);
 require('./behavior/joke.js')(bot);
+require('./behavior/kees.js')(bot);
+require('./behavior/me-irl.js')(bot);
+require('./behavior/question.js')(bot);
+require('./behavior/sarcasm.js')(bot);
