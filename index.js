05ae125a50ba44b340c9b6e01b161ab997ff2905
indri-indri <rutger@codeyellow.nl>
2016-09-28 23:15:53 +0200

    Add Eslint

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const bot = new TelegramBot(token, {
 const theEnd = moment('2017-02-10T12:11:42+00:00');
 const conversations = [];
 
-bot.onText(/kees/i, function (msg, match) {
+bot.onText(/kees/i, msg => {
 	let resp = 'rot gewoon op joh kk joch';
 
 	if (msg.text.match(/moet/ig)) {
@@ -34,7 +34,7 @@ bot.onText(/kees/i, function (msg, match) {
 	bot.sendMessage(msg.chat.id, resp);
 });
 
-bot.on('message', function (msg) {
+bot.on('message', msg => {
 	const chatId = msg.chat.id;
 	const userId = msg.from.id;
 
@@ -67,41 +67,41 @@ bot.on('message', function (msg) {
 	}
 });
 
-bot.onText(/communis/i, function (msg, match) {
+bot.onText(/communis/i, msg => {
 	const resp = '☭☭☭ RELIGIE IS DE OPIUM VAN HET VOLK ☭☭☭';
 	bot.sendMessage(msg.chat.id, resp);
 });
 
-bot.onText(/apple/i, function (msg, match) {
+bot.onText(/apple/i, msg => {
 	const resp = 'rip steve';
 	bot.sendMessage(msg.chat.id, resp);
 });
 
-bot.onText(/me.*irl/i, function (msg, match) {
+bot.onText(/me.*irl/i, msg => {
 	const resp = 'me too thanks';
 	bot.sendMessage(msg.chat.id, resp);
 });
 
-bot.onText(/;p\[/, function (msg, match) {
+bot.onText(/;p\[/, msg => {
 	bot.sendSticker(msg.chat.id, 'BQADBAADOQEAAqBWfgAB2V2T9yivgk8C');
 });
 
-bot.onText(/doei/i, function (msg, match) {
+bot.onText(/doei/i, msg => {
 	const audio = fs.readFileSync('sounds/doeihe.wav');
 	bot.sendVoice(msg.chat.id, audio);
 });
 
-bot.onText(/^inb4/i, function (msg, match) {
+bot.onText(/^inb4/i, msg => {
 	const resp = 'inb4 opkankeren';
 	bot.sendMessage(msg.chat.id, resp);
 });
 
-bot.onText(/\b(grap)\b/i, function (msg, match) {
+bot.onText(/\b(grap)\b/i, msg => {
 	const resp = 'je LEVEN is een grap!';
 	bot.sendMessage(msg.chat.id, resp);
 });
 
-bot.onText(/\?$/, function (msg, match) {
+bot.onText(/\?$/, msg => {
 	const textClean = msg.text.replace(/[^a-zA-Z0-9 ]/g, '');
 	const query = textClean.replace(/\s+/g, '+');
 	const url = `http://www.google.com/search?q=${query}&btnI`;
@@ -121,13 +121,13 @@ bot.onText(/\?$/, function (msg, match) {
 		});
 });
 
-bot.onText(/lijden/i, function (msg, match) {
-		const now = moment();
+bot.onText(/lijden/i, msg => {
+	const now = moment();
 
-		const daysLeft = theEnd.diff(now, 'days');
-		const monthsLeft = theEnd.diff(now, 'months', true);
+	const daysLeft = theEnd.diff(now, 'days');
+	const monthsLeft = theEnd.diff(now, 'months', true);
 
-		const resp = `Nog ${daysLeft} dagen, of ${monthsLeft.toFixed(1)} maanden tot De Wedergeboorte op 10 februari 2017.`;
+	const resp = `Nog ${daysLeft} dagen, of ${monthsLeft.toFixed(1)} maanden tot De Wedergeboorte op 10 februari 2017.`;
 
-		bot.sendMessage(msg.chat.id, resp);
+	bot.sendMessage(msg.chat.id, resp);
 });
