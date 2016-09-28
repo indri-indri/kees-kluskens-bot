16e72ba94bae7f87f8c22a6404014e3474bb040a
indri-indri <rutger@codeyellow.nl>
2016-09-28 22:20:34 +0200

    Add editorconfig
    Tabs > spaces

--- index.js
+++ index.js
@@ -11,123 +11,123 @@ moment.locale('nl');
 
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, {
-  polling: true
+	polling: true
 });
 const theEnd = moment('2017-02-10T12:11:42+00:00');
 const conversations = [];
 
 bot.onText(/kees/i, function (msg, match) {
-  let resp = 'rot gewoon op joh kk joch';
+	let resp = 'rot gewoon op joh kk joch';
 
-  if (msg.text.match(/moet/ig)) {
-    if (msg.text.match(/ik/ig)) {
-      if (Math.random() < .5) {
-        resp = 'je doet maar!';
-      } else {
-        resp = 'ben je mal!';
-      }
-    } else {
-      resp = 'ik moet helemaal niks!';
-    }
-  }
+	if (msg.text.match(/moet/ig)) {
+		if (msg.text.match(/ik/ig)) {
+			if (Math.random() < .5) {
+				resp = 'je doet maar!';
+			} else {
+				resp = 'ben je mal!';
+			}
+		} else {
+			resp = 'ik moet helemaal niks!';
+		}
+	}
 
-  bot.sendMessage(msg.chat.id, resp);
+	bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.on('message', function (msg) {
-  const chatId = msg.chat.id;
-  const userId = msg.from.id;
+	const chatId = msg.chat.id;
+	const userId = msg.from.id;
 
-  let conversation = _.find(conversations, ['chatId', msg.chat.id]);
+	let conversation = _.find(conversations, ['chatId', msg.chat.id]);
 
-  if (!conversation) {
-    conversations.push({
-      chatId,
-      lastMessage: {
-        userId,
-        count: 0,
-      }
-    });
+	if (!conversation) {
+		conversations.push({
+			chatId,
+			lastMessage: {
+				userId,
+				count: 0,
+			}
+		});
 
-    conversation = _.find(conversations, ['chatId', msg.chat.id]);
-  }
+		conversation = _.find(conversations, ['chatId', msg.chat.id]);
+	}
 
-  if (conversation.lastMessage.userId === userId) {
-    conversation.lastMessage.count++;
-  } else {
-    conversation.lastMessage = {
-      userId,
-      count: 1,
-    };
-  }
+	if (conversation.lastMessage.userId === userId) {
+		conversation.lastMessage.count++;
+	} else {
+		conversation.lastMessage = {
+			userId,
+			count: 1,
+		};
+	}
 
-  if (conversation.lastMessage.count === 5) {
-    const resp = 'KK OP MET JE SPAM';
-    bot.sendMessage(msg.chat.id, resp);
-  }
+	if (conversation.lastMessage.count === 5) {
+		const resp = 'KK OP MET JE SPAM';
+		bot.sendMessage(msg.chat.id, resp);
+	}
 });
 
 bot.onText(/communis/i, function (msg, match) {
-  const resp = '☭☭☭ RELIGIE IS DE OPIUM VAN HET VOLK ☭☭☭';
-  bot.sendMessage(msg.chat.id, resp);
+	const resp = '☭☭☭ RELIGIE IS DE OPIUM VAN HET VOLK ☭☭☭';
+	bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.onText(/apple/i, function (msg, match) {
-  const resp = 'rip steve';
-  bot.sendMessage(msg.chat.id, resp);
+	const resp = 'rip steve';
+	bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.onText(/me.*irl/i, function (msg, match) {
-  const resp = 'me too thanks';
-  bot.sendMessage(msg.chat.id, resp);
+	const resp = 'me too thanks';
+	bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.onText(/;p\[/, function (msg, match) {
-  bot.sendSticker(msg.chat.id, 'BQADBAADOQEAAqBWfgAB2V2T9yivgk8C');
+	bot.sendSticker(msg.chat.id, 'BQADBAADOQEAAqBWfgAB2V2T9yivgk8C');
 });
 
 bot.onText(/doei/i, function (msg, match) {
-  const audio = fs.readFileSync('sounds/doeihe.wav');
-  bot.sendVoice(msg.chat.id, audio);
+	const audio = fs.readFileSync('sounds/doeihe.wav');
+	bot.sendVoice(msg.chat.id, audio);
 });
 
 bot.onText(/^inb4/i, function (msg, match) {
-  const resp = 'inb4 opkankeren';
-  bot.sendMessage(msg.chat.id, resp);
+	const resp = 'inb4 opkankeren';
+	bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.onText(/\b(grap)\b/i, function (msg, match) {
-  const resp = 'je LEVEN is een grap!';
-  bot.sendMessage(msg.chat.id, resp);
+	const resp = 'je LEVEN is een grap!';
+	bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.onText(/\?$/, function (msg, match) {
-  const textClean = msg.text.replace(/[^a-zA-Z0-9 ]/g, '');
-  const query = textClean.replace(/\s+/g, '+');
-  const url = `http://www.google.com/search?q=${query}&btnI`;
+	const textClean = msg.text.replace(/[^a-zA-Z0-9 ]/g, '');
+	const query = textClean.replace(/\s+/g, '+');
+	const url = `http://www.google.com/search?q=${query}&btnI`;
 
-  got(url)
-    .then(response => {
-      const responseUrl = response.socket._httpMessage.socket._httpMessage.socket._httpMessage.res.url;
+	got(url)
+		.then(response => {
+			const responseUrl = response.socket._httpMessage.socket._httpMessage.socket._httpMessage.res.url;
 
-      if (responseUrl.match(/google/ig)) {
-        return;
-      }
+			if (responseUrl.match(/google/ig)) {
+				return;
+			}
 
-      const resp = `[ ](${responseUrl})`;
-      bot.sendMessage(msg.chat.id, resp, {
-        parse_mode: 'Markdown',
-      });
-    });
+			const resp = `[ ](${responseUrl})`;
+			bot.sendMessage(msg.chat.id, resp, {
+				parse_mode: 'Markdown',
+			});
+		});
 });
 
 bot.onText(/lijden/i, function (msg, match) {
-    const now = moment();
+		const now = moment();
 
-    const daysLeft = theEnd.diff(now, 'days');
-    const monthsLeft = theEnd.diff(now, 'months', true);
+		const daysLeft = theEnd.diff(now, 'days');
+		const monthsLeft = theEnd.diff(now, 'months', true);
 
-    const resp = `Nog ${daysLeft} dagen, of ${monthsLeft.toFixed(1)} maanden tot De Wedergeboorte op 10 februari 2017.`;
+		const resp = `Nog ${daysLeft} dagen, of ${monthsLeft.toFixed(1)} maanden tot De Wedergeboorte op 10 februari 2017.`;
 
-    bot.sendMessage(msg.chat.id, resp);
+		bot.sendMessage(msg.chat.id, resp);
 });
