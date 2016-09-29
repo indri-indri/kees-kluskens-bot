72ff19c09292d3bf2bc7b3daa3a34500bed0c12c
indri-indri <rma.schimmel@gmail.com>
2016-09-29 15:45:17 +0200

    Add highly creative insults

--- behavior/kees.js
+++ behavior/kees.js
@@ -1,9 +1,13 @@
+function randomElement (array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
+
 module.exports = function (bot) {
 	bot.onText(/^kees/i, msg => {
-		let resp = 'rot gewoon op joh kk joch';
+		let resp;
 
-		if (msg.text.match(/moet/ig)) {
-			if (msg.text.match(/ik/ig)) {
+		if (msg.text.match(/moet/i)) {
+			if (msg.text.match(/ik/i)) {
 				if (Math.random() < .5) {
 					resp = 'je doet maar!';
 				} else {
@@ -12,6 +16,60 @@ module.exports = function (bot) {
 			} else {
 				resp = 'ik moet helemaal niks!';
 			}
+		} else {
+			'rot gewoon op joh kk joch';
+
+			const go = [
+				'rot ',
+				'kk ',
+				'tief ',
+				'flikker ',
+			];
+
+			const just = [
+				'gwn ',
+				'gewoon ',
+				'',
+			];
+
+			const distance = [
+				'',
+				'een eind ',
+			];
+
+			const man = [
+				' joh',
+				' gast',
+				' man',
+				'',
+			];
+
+			const separator = [
+				', ',
+				' ',
+			];
+
+			const disease = [
+				'kk',
+				'tyfus',
+				'tering',
+			];
+
+			const space = [
+				' ',
+				'',
+			];
+
+			const person = [
+				'joch',
+				'jong',
+				'kind',
+				'debiel',
+				'mongool',
+				'mongooltje',
+			];
+
+			resp = randomElement(go) + randomElement(just) + randomElement(distance) + 'op' + randomElement(man) + randomElement(separator) + randomElement(disease) + randomElement(space) + randomElement(person);
 		}
 
 		bot.sendMessage(msg.chat.id, resp);
