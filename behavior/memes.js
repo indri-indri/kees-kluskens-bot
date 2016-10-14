54c241e74e84b818d32318a657355da9533a9bab
Rutger Schimmel <rma.schimmel@gmail.com>
2016-10-14 15:16:59 +0200

    Add zullen we daar een app voor maken

--- behavior/memes.js
+++ behavior/memes.js
@@ -14,4 +14,8 @@ module.exports = function (bot) {
 		const resp = 'ik ook bedankt';
 		bot.sendMessage(msg.chat.id, resp);
 	});
+	bot.onText(/idee/i, msg => {
+		const resp = 'zullen we daar een app voor maken';
+		bot.sendMessage(msg.chat.id, resp);
+	});
 };
