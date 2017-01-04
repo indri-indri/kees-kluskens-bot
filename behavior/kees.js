d06e7ba0e5fdd4912ca33a4e5631a5c11f2a9c97
JasperStam <jasper@strnk.nl>
2017-01-04 16:12:53 +0100

    Update kees_irl

--- behavior/kees.js
+++ behavior/kees.js
@@ -75,7 +75,7 @@ module.exports = function (bot) {
 		} else if (msg.text.match(/\b(kun|kan|wil)\b/i)) {
 			resp = 'ik ben bezig met een webpack core meeting';
 		} else {
-			resp = Math.random() > .5 ? 'mavic pro' : getSwearReaction();
+			resp = Math.random() > .5 ? '*mijn drone footage' : getSwearReaction();
 		}
 
 		bot.sendMessage(msg.chat.id, resp);
