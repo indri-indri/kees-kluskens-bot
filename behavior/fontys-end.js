bbb54a8f8b42424949f34833f5250567e30b7c8e
Rutger Schimmel <info@rutgerschimmel.nl>
2018-04-05 09:41:59 +0200

    Make `lijden` great again

--- behavior/fontys-end.js
+++ behavior/fontys-end.js
@@ -1,32 +1,7 @@
-const moment = require('moment');
-
-const theEnd = moment('2017-01-28T00:00:00+00:00');
-
-module.exports = function (bot) {
+module.exports = function(bot) {
 	bot.onText(/lijden/i, msg => {
-		const now = moment();
-
-		const daysLeft = theEnd.diff(now, 'days');
-		const monthsLeft = theEnd.diff(now, 'months', true);
-		
-		let resp = `nog ${daysLeft} dagen (${monthsLeft.toFixed(1)} maanden) tot de WEDERGEBOORTE op 28 januari 2017`;
-		
-		if (daysLeft < 1) {
-			resp = 'Vive la révolution!';
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-			bot.sendSticker(msg.chat.id, 'CAADAgADYgMAAvoLtghdGzKPtFIboQI');
-		}
-
-		bot.sendMessage(msg.chat.id, resp);
+		bot.sendSticker(msg.chat.id, 'CAADAgADggEAAooSqg6SPWMt4t_5BAI');
+		bot.sendSticker(msg.chat.id, 'CAADAgADggEAAooSqg6SPWMt4t_5BAI');
+		bot.sendSticker(msg.chat.id, 'CAADAgADggEAAooSqg6SPWMt4t_5BAI');
 	});
 };
